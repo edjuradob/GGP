da6b27897baa5ad9995d4a9d5e1ab0458e44e4de
Edgar <edgar.jurado@gonet.us>
2015-04-27 17:01:35 -0500

    deliberator, first approach

--- src/main/java/org/ggp/base/player/gamer/coursera/statemachine/Deliberator.java
+++ src/main/java/org/ggp/base/player/gamer/coursera/statemachine/Deliberator.java
@@ -9,6 +9,7 @@ import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
 import org.ggp.base.util.game.Game;
 import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.Role;
 import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.cache.CachedStateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
@@ -38,36 +39,83 @@ public class Deliberator extends StateMachineGamer {
 		// TODO Auto-generated method stub
 		long start = System.currentTimeMillis();
 		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
-		Move selection = bestMove(moves);
+		Move selection = bestMove(getCurrentState(), getRole());
 
 		long stop = System.currentTimeMillis();
+
 
 		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
 		return selection;
 	}
 
-	private int maxScore(List<Move> moves) throws GoalDefinitionException, TransitionDefinitionException{
+	private int maxScore(MachineState state, Role role) throws GoalDefinitionException, TransitionDefinitionException, MoveDefinitionException{
 		int score=0;
-		if (getStateMachine().isTerminal(getCurrentState())){
-				//getGoal(getCurrentState(), getRole())==100){
-			return 100;
+		StateMachine lState=getStateMachine();
+		if (lState.isTerminal(state)){
+			return lState.getGoal(state, role);
 		}
-		MachineState state = getCurrentState();
-		for (Move move : moves) {
-			ArrayList<Move> lMoves=new ArrayList<Move>();
+		List<Move> legalMoves=lState.getLegalMoves(state, getRole());
+		MachineState simState;
+		List<Move> lMoves=new ArrayList<Move>();
+		for (Move move : legalMoves) {
 			lMoves.add(move);
-//			state=getNextState(state,lMoves);
-//			score=maxScore(state);
+			simState=getStateMachine().getNextState(state,lMoves);
+			int result=maxScore(simState, role);
+			if (result>score){
+				score=result;
+			}
 		}
 		return score;
-
 	}
 
 
 
-	private Move bestMove(List<Move> moves) {
-		// TODO Auto-generated method stub
-		return null;
+
+
+	private Move bestMove(MachineState state, Role role) {
+
+		StateMachine lState=getStateMachine();
+		Move move=null;
+		int score=0;
+		try {
+			List<Move> legalMoves=lState.getLegalMoves(state, getRole());
+			move=legalMoves.get(0);
+			if (move.getContents().toString()=="noop"){
+				return move;
+			}
+			if (lState.isTerminal(state)){
+				//return lState.getGoal(state, role);
+			}
+
+			for (Move iMove : legalMoves) {
+				List<Move> lMoves=new ArrayList<Move>();
+				lMoves.add(iMove);
+				MachineState simState=getStateMachine().getNextState(state,lMoves);
+				int result=maxScore(simState, role);
+				if (result==100){
+					move=iMove;
+					break;
+				}
+				if (result>score){
+					score=result;
+					move=iMove;
+				}
+			}
+
+		} catch (MoveDefinitionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransitionDefinitionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GoalDefinitionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
+		return move;
 	}
 
 	@Override
